e881d6ac2e21f0da1833ee1cc667678b481a68aa
ankitsharma1999 <ank.shrma.1999@gmail.com>
2019-04-21 16:55:42 +0530

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from res import caesar_gen
+from res import caesar_gen, final_msg
 
 class message(str):
     def __init__(self,msg):
@@ -8,9 +8,23 @@ class message(str):
     def encrypt(self):
         new_m = caesar_gen(msg)
         return new_m
+    
+    def decrypt(self):
+        new_m = final_msg(msg)
+        return new_m
 
 
 if __name__ == "__main__":
-    m = input("Enter a message: ")
+
+    m = input("Enter the mesaage: ")
     msg = message(m)
-    print(msg.encrypt())
+
+    print("1.Encrypt\n2.Decrypt")
+        
+    ch = input("Enter your choice(1/2): ")
+    if ch=="1":
+        print(msg.encrypt())
+    elif ch=="2":
+        print(msg.decrypt())
+    else:
+        print("Enter a valid choice.")
