760e8ce557c5236ccb4544cb05ab83b821178580
ankitsharma1999 <ank.shrma.1999@gmail.com>
2019-05-20 08:59:44 +0530

    UPDATED

--- main.py
+++ main.py
@@ -18,32 +18,24 @@ class message(str):    # class message has all the attributes of string class
 if __name__ == "__main__":
 
     m = input("Enter the message: ")
-
     msg = message(m)
 
     print("1.Encrypt\n2.Decrypt")
-        
     ch = input("Enter your choice(1/2): ")
 
     while True:
-
         try:
-
             key = int(input("Enter the Key: "))
             break
         
         except:
-            
             print("Enter A Valid Key ")
 
     if ch=="1":
-
         print(msg.encrypt(key))
 
     elif ch=="2":
-
         print(msg.decrypt(key))
 
     else:
-
         print("Enter a valid choice.")
